d862e42ba9c670c18320c4c2507d3829b1d7979a
yerimya <syl1602@naver.com>
2023-12-04 00:19:37 +0900

    feat: 회원 관리- 웹 MVC 개발

---
+++ src/main/java/choonsik/choonsikspring/controller/MemberForm.java
@@ -0,0 +1,12 @@
+package choonsik.choonsikspring.controller;
+
+public class MemberForm {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
