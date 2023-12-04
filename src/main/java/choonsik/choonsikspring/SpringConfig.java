9f83c10ce3e62ab3abaa810bda875f66eab0aa24
yerimya <syl1602@naver.com>
2023-12-04 14:15:42 +0900

    feat: jdbc

--- src/main/java/choonsik/choonsikspring/SpringConfig.java
+++ src/main/java/choonsik/choonsikspring/SpringConfig.java
@@ -1,13 +1,24 @@
 package choonsik.choonsikspring;
 
+import choonsik.choonsikspring.repository.JdbcMemberRepository;
+import choonsik.choonsikspring.repository.JdbcTemplateMemberRepository;
 import choonsik.choonsikspring.repository.MemberRepository;
 import choonsik.choonsikspring.repository.MemoryMemberRepository;
 import choonsik.choonsikspring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -15,7 +26,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        //return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
         // interface는 new 안됨.
     }
 }
